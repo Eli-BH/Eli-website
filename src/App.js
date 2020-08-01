42b3a8272cf4b96f69cc8dd1d9349aeb846c079a
Eli-BH <eli.i.henderson95@gmail.com>
2020-07-31 23:25:38 -0400

    added darkmode switch and icon

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from "react";
-
+import React, { useState } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Divider from "@material-ui/core/Divider";
 import Drawer from "@material-ui/core/Drawer";
 import Hidden from "@material-ui/core/Hidden";
 import IconButton from "@material-ui/core/IconButton";
-
 import MenuIcon from "@material-ui/icons/Menu";
-
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import { Typography } from "@material-ui/core";
+import {
+  makeStyles,
+  useTheme,
+  ThemeProvider,
+  createMuiTheme,
+} from "@material-ui/core/styles";
+import { Typography, Switch } from "@material-ui/core";
 import MainPage from "./components/MainPage";
 import Sidebar from "./components/Sidebar";
-
 import Toolbar from "@material-ui/core/Toolbar";
 import Box from "@material-ui/core/Box";
+import Paper from "@material-ui/core/Paper";
+import WbSunnyIcon from "@material-ui/icons/WbSunny";
+import NightsStayIcon from "@material-ui/icons/NightsStay";
 
 const drawerWidth = 240;
 
@@ -51,11 +55,14 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+//App starts here
 function ResponsiveDrawer(props) {
   const { window } = props;
   const classes = useStyles();
   const theme = useTheme();
   const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+  const styleIcon = !darkMode ? <NightsStayIcon /> : <WbSunnyIcon />;
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
@@ -69,69 +76,82 @@ function ResponsiveDrawer(props) {
       <Divider />
 
       <Divider />
+      <div style={{ direction: "flex" }}>
+        {styleIcon}
+        <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+      </div>
     </div>
   );
+
+  const styles = createMuiTheme({
+    palette: {
+      type: darkMode ? "dark" : "light",
+    },
+  });
 
   const container =
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <Box component="span" position="fixed">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            className={classes.menuButton}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </Box>
+    <ThemeProvider theme={styles}>
+      <Paper className={classes.root}>
+        <CssBaseline />
+        <Box component="span" position="fixed">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </Box>
 
-      <nav className={classes.drawer} aria-label="mailbox folders">
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Hidden smUp implementation="css">
-          <Drawer
-            container={container}
-            variant="temporary"
-            anchor={theme.direction === "rtl" ? "right" : "left"}
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
-        <Hidden xsDown implementation="css">
-          <Drawer
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            variant="permanent"
-            open
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
-      </nav>
-      <div>
-        <div style={{ height: "100vh" }}>
-          <MainPage />
+        <nav className={classes.drawer} aria-label="mailbox folders">
+          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+          <Hidden smUp implementation="css">
+            <Drawer
+              container={container}
+              variant="temporary"
+              anchor={theme.direction === "rtl" ? "right" : "left"}
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden xsDown implementation="css">
+            <Drawer
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              variant="permanent"
+              open
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+        </nav>
+
+        <div>
+          <div style={{ height: "100vh" }}>
+            <MainPage />
+          </div>
+          <div style={{ height: "100vh" }}>
+            <Sidebar />
+          </div>
         </div>
-        <div style={{ height: "100vh" }}>
-          <Sidebar />
-        </div>
-      </div>
-    </div>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
